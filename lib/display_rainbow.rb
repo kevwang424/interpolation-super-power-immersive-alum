c7eb4ca78e88ef56831e81aff188e46eb31d7607
Kevin Wang <kev.wang424@gmail.com>
2018-02-10 17:15:48 -0500

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
 # Write your #display_rainbow method here
+def capitalize_first(word)
+  return word[0].upcase
+end
+
+def display_rainbow(colors)
+  puts "#{capitalize_first(colors[0])}: #{colors[0]}, #{capitalize_first(colors[1])}: #{colors[1]}, #{capitalize_first(colors[2])}: #{colors[2]}, #{capitalize_first(colors[3])}: #{colors[3]}, #{capitalize_first(colors[4])}: #{colors[4]}, #{capitalize_first(colors[5])}: #{colors[5]}, #{capitalize_first(colors[6])}: #{colors[6]}"
+end
